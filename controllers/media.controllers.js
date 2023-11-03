2a50a6c74f645fdd182ea3cddd586a37238a20c2
Putri Indriyanti <putri.indriyanti21@students.unila.ac.id>
2023-11-03 10:10:55 +0700

    add get image by id

--- controllers/media.controllers.js
+++ controllers/media.controllers.js
@@ -7,36 +7,44 @@ module.exports = {
     createImage: async (req,res,next) =>{
         try{
             let {judul, deskripsi} = req.body
-            let strFile = req.file.buffer.toString('base64');
+            try{
+                let strFile = req.file.buffer.toString('base64');
 
-            let { url } = await imagekit.upload({
-                fileName: Date.now() + path.extname(req.file.originalname),
-                file: strFile
-            });
-            
-
-            let image = await prisma.post.create ({
-                data :{
-                    judul,
-                    deskripsi,
-                    data_gambar: url
-                } 
-            });
-
-            return res.status(201).json({
-                status: true,
-                message : 'create image',
-                err: null,
-                data: {image}
-            }); 
+                let { url } = await imagekit.upload({
+                    fileName: Date.now() + path.extname(req.file.originalname),
+                    file: strFile
+                });
+                
+    
+                let image = await prisma.post.create ({
+                    data :{
+                        judul,
+                        deskripsi,
+                        data_gambar: url
+                    } 
+                });
+    
+                return res.status(201).json({
+                    status: true,
+                    message : 'create image',
+                    err: null,
+                    data: {image}
+                }); 
+            } catch (err) {
+                return res.status(400).json({
+                    status: false,
+                    message: err.message,
+                    data: null
+                });
+            }
         } catch (err) {
             next(err);
         }
     },
 
+
     getAllImage: async (req, res, next) =>{
         try{
-            //let {judul, deskripsi, data_gambar} = req.body
             
             let allImage = await prisma.post.findMany ({
                 select :{
@@ -60,6 +68,36 @@ module.exports = {
                 message:   err.message,
                 data: null
             });
+        }
+    },
+
+    getImageById: async (req, res, next) =>{
+        try{
+            const {id} = req.params;
+            try{
+                let detailImage = await prisma.post.findUnique ({
+                    where : {
+                        id :(Number(id))
+                    }
+                });
+
+                return res.status(200).json({
+                    status: true,
+                    message : 'OK',
+                    err: null,
+                    data: detailImage
+                }); 
+                
+            }catch (err) {
+                return res.status(400).json({
+                    status: false,
+                    message:   err.message,
+                    data: null
+                });
+            }
+
+        } catch (err) {
+            next(err);
         }
     },
     
--- routes/media.routes.js
+++ routes/media.routes.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 
 const {image} = require('../libs/multer');
-const {createImage, getAllImage} = require('../controllers/media.controllers');
+const {createImage, getAllImage, getImageById} = require('../controllers/media.controllers');
 
 router.post('/imagekit/create', image.single('data_gambar'), createImage);
 
 router.put('/imagekit/images', getAllImage);
+
+router.put('/imagekit/images/:id', getImageById);
+
+
 
 
 module.exports = router;
